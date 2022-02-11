ecd93504ffe86a8f2924f1b17f88c5cecdb52757
FRAKHDZ <48639189+FRAKHDZ@users.noreply.github.com>
2022-02-11 01:56:23 -0600

    Batch file to automate running python file

---
+++ autoRun.bat
@@ -0,0 +1,4 @@
+@echo off
+cd/
+Rem "Path to python.exe" "path to python file"
+"Python310\python.exe" "C:\Users\robbe\OneDrive - Northern Illinois University\dropp\code\py\auto_bing\search_works.py"
